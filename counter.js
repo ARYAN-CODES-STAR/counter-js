957bd75f9acdeae92abcb3a9d3d68f336ed9b4b2
Aryan Sharma <78686630+ARYAN-CODES-STAR@users.noreply.github.com>
2023-06-03 12:03:20 +0530

    Add files via upload

---
+++ counter.js
@@ -0,0 +1,30 @@
+const mainTitle = document.querySelector("#title");
+let curValue = 0;
+
+const btnDecrement = document.querySelector('#decrement');
+const btnReset = document.querySelector('#reset');
+const btnIncrement = document.querySelector('#increment');
+
+
+btnDecrement.addEventListener('click',() => {
+    curValue--;
+
+    if(curValue < 0 ){
+        curValue = 0;
+        mainTitle.textContent = curValue;
+    }
+    mainTitle.textContent = curValue;
+
+
+});
+
+btnIncrement.addEventListener("click", () => {
+    curValue++;
+    mainTitle.textContent = curValue;
+
+})
+
+btnReset.addEventListener("click",() => {
+    curValue = 0;
+    mainTitle.textContent = curValue;
+})
